d16d34b280a5cd2a2c21203394bbd09d3752c5a3
bartixoto <bastianmore18@gmail.com>
2025-09-26 12:37:05 -0300

    clase 2

---
+++ diagramamio.py
@@ -0,0 +1,21 @@
+class :
+    def __init__(self, nombre: str,edad: int,numero_inscripcion: str):
+        self._nombre: str = nombre
+        self._edad: int = edad
+        self._numero_inscripcion: str = numero_inscripcion
+        pass
+class Entrenador:
+    def __init__(self,equipos: str,planes_entrenamiento: str):
+        self.equipos: str = equipos
+        self._planes_entrenamiento: str = planes_entrenamiento
+    pass
+
+class Atleta:
+    def __init__(self, diciplinas: str, marcas: str):
+        self.diciplinas: str = diciplinas
+        self.marcas: str = marcas
+    pass
+
+class Juez:
+    def __init__(self,)
+    pass
